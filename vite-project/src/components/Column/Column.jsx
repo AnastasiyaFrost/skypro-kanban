bdb980ed71f562d8ea68fa5f5dcfa009c4bba57c
user <grebneva.nastena@yandex.ru>
2024-02-12 22:17:23 +0300

    +Card, Column, PopBrowse, Header, Main components

---
+++ vite-project/src/components/Column/Column.jsx
@@ -0,0 +1,18 @@
+import Card from "../Card/Card";
+
+export default function Column ({title}) {
+    return (
+      <div className="main__column">
+        <div className="column__title">
+          <p>{title}</p>
+        </div>
+        <div className="cards">
+          <Card topic={"Web Design"} title={"Новая задача"} />
+          <Card topic={"Research"} title={"Новая задача"} />
+          <Card topic={"Web Design"} title={"Новая задача"} />
+          <Card topic={"Testing"} title={"Новая задача"} />
+          <Card topic={"SMM"} title={"Новая задача"} />
+        </div>
+      </div>
+    );
+}
