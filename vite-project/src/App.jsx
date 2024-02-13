5dc6366b6e9f236cff5e151c28c76a0dc7bcbad2
user <grebneva.nastena@yandex.ru>
2024-02-13 20:03:28 +0300

    hw-1

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,29 +1,33 @@
 //import { useState } from 'react'
 //import reactLogo from './assets/react.svg'
 //import viteLogo from '/vite.svg'
-import PopExit from '/src/components/popups/PopExit/PopExit.jsx'
-import PopNewCard from '/src/components/popups/PopNewCard/PopNewCard.jsx'
-import PopBrowse from './components/popups/PopBrowse/PopBrowse';
-import Header from './components/Header/Header';
-import Main from './components/Main/Main';
-import './App.css'
-
+import PopExit from "/src/components/popups/PopExit/PopExit.jsx";
+import PopNewCard from "/src/components/popups/PopNewCard/PopNewCard.jsx";
+import PopBrowse from "./components/popups/PopBrowse/PopBrowse";
+import Header from "./components/Header/Header";
+import MainContent from "./components/MainContent/MainContent";
+import "./App.css";
+import Column from "./components/Column/Column";
 
 export default function App() {
- 
   return (
     <>
-      
       <div className="wrapper">
-        <PopExit/>
+        <PopExit />
 
-        <PopNewCard/>
+        <PopNewCard />
 
-        <PopBrowse/>
+        <PopBrowse />
 
-        <Header/>
+        <Header />
 
-        <Main/>
+        <MainContent>
+          <Column title={"Без статуса"} />
+          <Column title={"Нужно сделать"} />
+          <Column title={"В работе"} />
+          <Column title={"Тестирование"} />
+          <Column title={"Готово"} />
+        </MainContent>
       </div>
     </>
   );
